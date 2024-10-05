b9e0115519405b77f1b82e73d14ce5f0dfe4ea01
Zhang-Yu001 <1923299134@qq.com>
2024-10-05 18:02:51 +0800

    third commit

--- LeetCode 热题 100/2.py
+++ LeetCode 热题 100/2.py
@@ -2,32 +2,44 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> results;
-        vector<string> s;
         unordered_map<string, vector<string>> hashtable;
-        unordered_map<string, int> hash;
-        for(int i = 0; i < strs.size(); i++){
-            string str = strs[i];
-            sort(str.begin(), str.end());
-            s.push_back(str);
-            auto it = hashtable.find(str);
-            if(it != hashtable.end()){
-                it->second.push_back(strs[i]);
-            }
-            else{
-                vector<string> result;
-                result.push_back(strs[i]);
-                hashtable[str] = result;
-            }
+        for(string& str : strs){
+            string key = str;
+            sort(key.begin(), key.end());
+            hashtable[key].emplace_back(str);
         }
-        for(int i = 0; i < s.size(); i++){
-            auto it = hash.find(s[i]);
-            if(it == hash.end()){
-                results.push_back(hashtable.find(s[i])->second);
-                hash[s[i]] = i;
-            }
+        for(auto it : hashtable){
+            results.emplace_back(it.second);
         }
         return results;
         
+        # vector<vector<string>> results;
+        # vector<string> s;
+        # unordered_map<string, vector<string>> hashtable;
+        # unordered_map<string, int> hash;
+        # for(int i = 0; i < strs.size(); i++){
+        #     string str = strs[i];
+        #     sort(str.begin(), str.end());
+        #     s.push_back(str);
+        #     auto it = hashtable.find(str);
+        #     if(it != hashtable.end()){
+        #         it->second.push_back(strs[i]);
+        #     }
+        #     else{
+        #         vector<string> result;
+        #         result.push_back(strs[i]);
+        #         hashtable[str] = result;
+        #     }
+        # }
+        # for(int i = 0; i < s.size(); i++){
+        #     auto it = hash.find(s[i]);
+        #     if(it == hash.end()){
+        #         results.push_back(hashtable.find(s[i])->second);
+        #         hash[s[i]] = i;
+        #     }
+        # }
+        # return results;
+
             # int len = strs.size();
             # vector<int> nums;
             # for(int i = 0; i < len; i++){
