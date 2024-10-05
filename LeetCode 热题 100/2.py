47b5ba41ebc912d7a001f810db8c1f7266704ead
Zhang-Yu001 <1923299134@qq.com>
2024-10-05 11:17:10 +0800

    second commit

--- LeetCode 热题 100/2.py
+++ LeetCode 热题 100/2.py
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int len = strs.size();
-        vector<int> nums;
-        for(int i = 0; i < len; i++){
-            string str = strs[i];
-            int num = 0;
-            for(int j = 0; j < str.length(); j++){
-                num += 1 << int(str[j] - 'a');
-            }
-            nums.push_back(num);
-        }
-        unordered_map<int, int> h;
-        vector<vector<string>> results;
-        for(int i = 0; i < len; i++){
-            auto it = h.find(nums[i]);
-            if(it == h.end()){
-                vector<string> result;
-                h[nums[i]] = i;
-                result.push_back(strs[i]);
-                for(int j = i + 1; j < len; j++){
-                    if(nums[j] == nums[i]){
-                        result.push_back(strs[j]);
-                    }
-                }
-                results.push_back(result);
-            }
-        }
-        return results;
+        # int len = strs.size();
+        # vector<int> nums;
+        # for(int i = 0; i < len; i++){
+        #     string str = strs[i];
+        #     int num = 0;
+        #     for(int j = 0; j < str.length(); j++){
+        #         num += 1 << int(str[j] - 'a');
+        #     }
+        #     nums.push_back(num);
+        # }
+        # unordered_map<int, int> h;
+        # vector<vector<string>> results;
+        # for(int i = 0; i < len; i++){
+        #     auto it = h.find(nums[i]);
+        #     if(it == h.end()){
+        #         vector<string> result;
+        #         h[nums[i]] = i;
+        #         result.push_back(strs[i]);
+        #         for(int j = i + 1; j < len; j++){
+        #             if(nums[j] == nums[i]){
+        #                 result.push_back(strs[j]);
+        #             }
+        #         }
+        #         results.push_back(result);
+        #     }
+        # }
+        # return results;
     }
 };
