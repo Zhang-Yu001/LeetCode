2f143ac3e60f7dbf6ea75a8f014221e160e47f80
Zhang-Yu001 <1923299134@qq.com>
2024-10-07 20:53:29 +0800

    second commit

--- LeetCode 热题 100/4.cpp
+++ LeetCode 热题 100/4.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int i = 0; i < nums.size() - 1; i++){
-            if(!nums[i]){
-                int j = i + 1;
-                while(1){
-                    if(!nums[j] && j != nums.size() - 1){
-                        j++;
-                    }
-                    else{
-                        break;
-                    }
-                }
-                swap(nums[i], nums[j]);
+        int left = 0, right = 0;
+        while(right < nums.size()){
+            if(nums[right]){
+                swap(nums[left], nums[right]);
+                left++;
             }
+            right++;
         }
+        // for(int i = 0; i < nums.size() - 1; i++){
+        //     if(!nums[i]){
+        //         int j = i + 1;
+        //         while(1){
+        //             if(!nums[j] && j != nums.size() - 1){
+        //                 j++;
+        //             }
+        //             else{
+        //                 break;
+        //             }
+        //         }
+        //         swap(nums[i], nums[j]);
+        //     }
+        // }
     }
 };
