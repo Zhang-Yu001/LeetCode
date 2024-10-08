d97211eedb62dd3fa610506410deb2aac1d1795e
Zhang-Yu001 <1923299134@qq.com>
2024-10-08 22:01:25 +0800

    second commit

--- LeetCode 热题 100/7.cpp
+++ LeetCode 热题 100/7.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        // 双指针
+        int n = height.size() - 1, res = 0;
+        int left = 0, right = n, leftmax = 0, rightmax = 0;
+        while(left < right){
+            leftmax = max(leftmax, height[left]);
+            rightmax = max(rightmax, height[right]);
+            if(leftmax < rightmax) res+= leftmax - height[left++];
+            else res += rightmax - height[right--];
+        }
+        return res;
+
         // 动态规划
         int n = height.size() - 1;
         int leftmax[20010], rightmax[20010];
@@ -16,37 +27,38 @@ public:
             res += min(leftmax[i], rightmax[i]) - height[i];
         }
         return res;
-
-        // int n = height.size();
-        // int left = 0, right = 0, res = 0;
-        // while(left < n - 1){
-        //     int cnt = 0;
-        //     while(!height[left]) left++;
-        //     right = left + 1;
-        //     while(right < n && height[right] < height[left]){
-        //         cnt += height[right++];
-        //     }
-        //     if(right == n && height[right - 1] < height[left]){
-        //         n = left - 1;
-        //         left = height.size() - 1;
-        //         right = height.size() - 1;
-        //         while(left > n + 1){
-        //             cnt = 0;
-        //             while(!height[left]) left--;
-        //             right = left - 1;
-        //             while(right > n && height[right] < height[left]){
-        //                 cnt += height[right--];
-        //             }
-        //             res += (left - right - 1) * height[left] - cnt;
-        //             left = right;
-        //         }
-        //         break;
-        //     }
-        //     else{
-        //         res += (right - left - 1) * height[left] - cnt;
-        //         left = right;
-        //     }
-        // }
-        // return res;
+        
+        // my solution
+        int n = height.size();
+        int left = 0, right = 0, res = 0;
+        while(left < n - 1){
+            int cnt = 0;
+            while(!height[left]) left++;
+            right = left + 1;
+            while(right < n && height[right] < height[left]){
+                cnt += height[right++];
+            }
+            if(right == n && height[right - 1] < height[left]){
+                n = left - 1;
+                left = height.size() - 1;
+                right = height.size() - 1;
+                while(left > n + 1){
+                    cnt = 0;
+                    while(!height[left]) left--;
+                    right = left - 1;
+                    while(right > n && height[right] < height[left]){
+                        cnt += height[right--];
+                    }
+                    res += (left - right - 1) * height[left] - cnt;
+                    left = right;
+                }
+                break;
+            }
+            else{
+                res += (right - left - 1) * height[left] - cnt;
+                left = right;
+            }
+        }
+        return res;
     }
 };
