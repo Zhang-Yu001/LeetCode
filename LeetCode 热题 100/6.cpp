fcf5b953860862047cd5b60cb852e5b4aed8de6d
Zhang-Yu001 <1923299134@qq.com>
2024-10-08 11:59:18 +0800

    second commit

---
+++ LeetCode 热题 100/6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 2; i++){
+            if(nums[i] > 0) break;
+            if(i != 0 && nums[i] == nums[i - 1]) continue;
+            int target = -nums[i], right = nums.size() - 1;
+            for(int left = i + 1; left < nums.size() - 1; left++){
+                if(left != i + 1 && nums[left] == nums[left - 1]) continue;
+                while(left < right && nums[left] + nums[right] > target) right--;
+                if(left == right) break;
+                if(nums[left] + nums[right] == target) res.push_back({nums[i], nums[left], nums[right]});
+            }
+        }
+        return res;
+    }
+};
--- LeetCode 热题 100/7.cpp
+++ LeetCode 热题 100/7.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int left = 0, right = 0, res = 0;
-        while(left < n - 1){
-            int cnt = 0;
-            while(!height[left]) left++;
-            right = left + 1;
-            while(right < n && height[right] < height[left]){
-                cnt += height[right++];
-            }
-            if(right == n && height[right - 1] < height[left]){
-                n = left - 1;
-                left = height.size() - 1;
-                right = height.size() - 1;
-                while(left > n + 1){
-                    cnt = 0;
-                    while(!height[left]) left--;
-                    right = left - 1;
-                    while(right > n && height[right] < height[left]){
-                        cnt += height[right--];
-                    }
-                    res += (left - right - 1) * height[left] - cnt;
-                    left = right;
-                }
-                break;
-            }
-            else{
-                res += (right - left - 1) * height[left] - cnt;
-                left = right;
-            }
+        // 动态规划
+        int n = height.size() - 1;
+        int leftmax[20010], rightmax[20010];
+        int res = 0;
+        leftmax[0] = height[0], rightmax[n] = height[n];
+        for(int i = 1; i <= n; i++){
+            leftmax[i] = max(leftmax[i - 1], height[i]);
+        }
+        for(int j = n - 1; j >= 0; j--){
+            rightmax[j] = max(rightmax[j + 1], height[j]);
+        }
+        for(int i = 0; i <= n; i++){
+            res += min(leftmax[i], rightmax[i]) - height[i];
         }
         return res;
+
+        // int n = height.size();
+        // int left = 0, right = 0, res = 0;
+        // while(left < n - 1){
+        //     int cnt = 0;
+        //     while(!height[left]) left++;
+        //     right = left + 1;
+        //     while(right < n && height[right] < height[left]){
+        //         cnt += height[right++];
+        //     }
+        //     if(right == n && height[right - 1] < height[left]){
+        //         n = left - 1;
+        //         left = height.size() - 1;
+        //         right = height.size() - 1;
+        //         while(left > n + 1){
+        //             cnt = 0;
+        //             while(!height[left]) left--;
+        //             right = left - 1;
+        //             while(right > n && height[right] < height[left]){
+        //                 cnt += height[right--];
+        //             }
+        //             res += (left - right - 1) * height[left] - cnt;
+        //             left = right;
+        //         }
+        //         break;
+        //     }
+        //     else{
+        //         res += (right - left - 1) * height[left] - cnt;
+        //         left = right;
+        //     }
+        // }
+        // return res;
     }
 };
